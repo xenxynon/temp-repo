9bb4383bbdcb48b8c4dfd4d7778f60664ce4e808
Frost <159105703+Karan-Frost@users.noreply.github.com>
2024-10-27 14:08:53 +0530

    veux: Update from V816.0.9.0.TKCMIXM
    Signed-off-by: Frost <159105703+Karan-Frost@users.noreply.github.com>

--- voltage_veux.mk
+++ voltage_veux.mk
@@ -32,6 +32,6 @@ PRODUCT_NAME := voltage_veux
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="veux_global-user 13 TKQ1.221114.001 V816.0.8.0.TKCMIXM release-keys" \
-    BuildFingerprint=Redmi/veux_global/veux:13/TKQ1.221114.001/V816.0.8.0.TKCMIXM:user/release-keys \
+    BuildDesc="veux_global-user 13 TKQ1.221114.001 V816.0.9.0.TKCMIXM release-keys" \
+    BuildFingerprint=Redmi/veux_global/veux:13/TKQ1.221114.001/V816.0.9.0.TKCMIXM:user/release-keys \
     DeviceProduct=veux_global
