9b72282ca9200d0b742020b9689afda2fcfede82
Snowman2409 <harshveera01@gmail.com>
2024-11-02 04:41:17 +0000

    veux: Welcome to ProjectPixelage

--- pixelage_veux.mk
+++ pixelage_veux.mk
@@ -11,23 +11,25 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device.
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-# Inherit some common VoltageOS stuff.
-$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
+# Inherit some common ProjectPixelage stuff.
+$(call inherit-product, vendor/pixelage/config/common_full_phone.mk)
 
-# VoltageOS flags.
-TARGET_FACE_UNLOCK_SUPPORTED := true
+# Inherit some common device props
 TARGET_USES_AOSP_RECOVERY := true
-# Official-ify.
-VOLTAGE_BUILD_TYPE := OFFICIAL
-
-# Bootanimation Resolution.
-TARGET_BOOT_ANIMATION_RES := 2160
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_ENABLE_BLUR := true
+TARGET_EXCLUDES_AUDIOFX := true
 
 PRODUCT_BRAND := Redmi
 PRODUCT_DEVICE := veux
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := 2201116SG
-PRODUCT_NAME := voltage_veux
+PRODUCT_NAME := pixelage_veux
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
